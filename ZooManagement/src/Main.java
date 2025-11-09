ad06332524327a181cfbf8fb3989a1f7cfa27824
unknown <kingmejaouel@gmail.com>
2025-11-09 21:33:53 +0100

    prosit 3

--- ZooManagement/src/Main.java
+++ ZooManagement/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args){
         Animal a = new Animal("lions","Asta",18,true);
         Zoo z = new Zoo("bouzalfa","bouzalfa");
-        System.out.println(a.toString());
-        System.out.println(z.toString());
+        z.addAnimal(a);
+
+        z.Display();
     }
 }
--- ZooManagement/src/Zoo.java
+++ ZooManagement/src/Zoo.java
@@ -4,13 +4,54 @@ public class Zoo {
     Animal[] animals;
     String name;
     String city;
-    int nbcg;
+    static final int nbmax=25;
+    int nbcg=0;
     public Zoo(String a,String b){
-        this.animals = new Animal[25];
+        this.animals = new Animal[nbmax];
         this.city=a;
         this.name=b;
     }
-
+    public boolean isFull(){
+        return nbcg >= nbmax;
+    }
+    public boolean addAnimal(Animal a){
+        if((nbcg>=25) || searchAnimal(a)!=-1){
+            System.out.println("Zoo is full,no cages!!!");
+        }
+        else{
+            animals[nbcg]=a;
+            nbcg++;
+            return true;
+        }
+        return false;
+    }
+    public void Display(){
+        for(int i=0;i<nbcg;i++){
+            System.out.println(animals[i]);
+        }
+    }
+    public int searchAnimal(Animal animal){
+        for(int i=0;i<nbcg;i++){
+            if(animals[i].name.equals(animal.name)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public boolean removeAnimal(Animal a){
+        int pos=searchAnimal(a);
+        if(searchAnimal(a)!=-1){
+            for (int i = pos; i < nbcg - 1; i++) {
+                animals[i] = animals[i + 1];
+            }
+            animals[--nbcg] = null;
+            return true;
+        }
+        return false;
+    }
+    public Zoo comparerZoo(Zoo z1,Zoo z2){
+        return (z1.nbcg > z2.nbcg) ? z1 : z2;
+    }
     @Override
     public String toString() {
         return "Zoo{" +
